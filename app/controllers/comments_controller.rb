80a0171d995e77a7a45b5498c938ccfb3847580d
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2020-09-14 23:03:27 +0200

    Add receiving arguments for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,8 +16,9 @@ class CommentsController < ApplicationController
     @post = Post.find(comment_params[:post_id])
     @comment = Comment.new
     @comment.post_id = @post.id
-    @comment.commenter_name = "Example Commenter"
-    @comment.content = "An eloquent retort."
+    @comment.commenter_name = comment_details[:commenter_name]
+    @comment.commenter_url = comment_details[:commenter_url]
+    @comment.content = comment_details[:content]
     if @comment.save
       redirect_to @post
     else
@@ -30,5 +31,9 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.permit(:id, :post_id, :authenticity_token, :commit)
+  end
+
+  def comment_details
+    params.require(:comment).permit(:commenter_name, :commenter_url, :content)
   end
 end
